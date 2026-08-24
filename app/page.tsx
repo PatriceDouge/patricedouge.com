7d912505b839fbdb46f0a4735f47449d560026fd
Patrice Douge <patricedouge@gmail.com>
2026-08-23 22:15:16 -0400

    copy cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,17 @@ export default function Home() {
 
         <header className="mb-16">
           <p className="text-muted leading-relaxed">
-            I&apos;m Patrice Dougé, a software engineer at <a href="https://wistia.com" className="text-foreground hover:underline underline-offset-2 transition-colors">Wistia</a>. Born in Pétion-Ville, Haiti, currently based in Sarasota, FL. These days I&apos;m learning as much as I can about coding agents, AI and how to build products users love. Most of my time I&apos;m either{" "}
+            I&apos;m Patrice Dougé, a software engineer at{" "}
+            <a
+              href="https://wistia.com"
+              className="text-foreground hover:underline underline-offset-2 transition-colors"
+            >
+              Wistia
+            </a>
+            . Born in Pétion-Ville, Haiti, currently based in Sarasota, FL.
+            These days I&apos;m learning as much as I can about coding agents,
+            AI and how to build products users love. Most of my time I&apos;m
+            either{" "}
             <a
               href="https://github.com/PatriceDouge"
               className="text-foreground hover:underline underline-offset-2 transition-colors"
@@ -41,11 +51,17 @@ export default function Home() {
           </p>
           <br />
           <p className="text-muted leading-relaxed">
-            It&apos;s an incredible time to be a software engineer. It&apos;s empowering to know how much leverage one has with AI tools, but it can also be overwhelming with all the different ways of doing things. There&apos;s an interesting tension between shipping beautiful code and shipping fast as long as it works.
+            It&apos;s an incredible time to be a software engineer. It&apos;s
+            empowering to know how much leverage we have with AI tools, but it
+            can also be overwhelming with all the different ways of doing
+            things. There&apos;s an interesting tension between shipping code
+            with craft and shipping fast as long as it works.
           </p>
           <br />
           <p className="text-muted leading-relaxed">
-            I&apos;m working to find that balance and continue growing as an engineer. This is where I share what I&apos;m learning along the way.
+            I&apos;m working to find that balance and continue growing as an
+            engineer. This is where I share what I&apos;m learning along the
+            way.
           </p>
         </header>
 
